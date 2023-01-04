7c3cd29f18a596d2ce897f8a140d5d6ce9b30393
glebite <glebite@gmail.com>
2023-01-03 20:26:37 -0500

    Bits and bits...

--- src/rgb_controls.py
+++ src/rgb_controls.py
@@ -27,8 +27,17 @@ class RAMWords:
             sys.exit(1)
 
     def _check_devices(self):
-        devices = self.cli.get_devices_by_type(DeviceType.DEVICE_NAME)[0]
+        self.devices = self.cli.get_devices_by_type(DeviceType.DEVICE_NAME)[0]
+        if len(self.devices) < MIN_DEVICES:
+            raise ValueError
 
+    def clear(self):
+        """
+        """
+        for device in self.devices:
+            # write BLACK (0,0,0) to the stick
+            pass
+        
     def fill_buffer(self, message):
         pass
 
