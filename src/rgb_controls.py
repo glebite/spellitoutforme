7abce87efdcd65f34809d61b4d0fb7ed7646d12d
glebite <glebite@gmail.com>
2023-01-04 18:45:46 -0500

    Adding colour definitions

--- src/rgb_controls.py
+++ src/rgb_controls.py
@@ -14,6 +14,14 @@ DEVICE_NAME = "Corsair Vengeance Pro RGB"
 MODE = "Direct"
 
 
+# colours:
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
 class RAMWords:
     """
     """
